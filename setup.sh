4b6bd5743556ecc4014bded7966d2a8d73c66f3c
taru-m <wwww.0909mizu@gmail.com>
2016-06-24 01:50:54 +0900

    環境変数名ミス

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #  sed -i -e "s/<USER>/$RDS_USER_STG/g" /etc/kong/kong.yml
 #  sed -i -e "s/<PASSWORD>/$RDS_PASSWORD_STG/g" < /etc/kong/kong.yml
 #elif [ -n "$ENV" -a "$ENV" = "pro" ]; then
-  sed -i -e "s/<HOST>/$RDS_HOST/g" /etc/kong/kong.yml
-  sed -i -e "s/<USER>/$RDS_USER/g" /etc/kong/kong.yml
+  sed -i -e "s/<HOST>/$RDS_HOSTNAME/g" /etc/kong/kong.yml
+  sed -i -e "s/<USER>/$RDS_USERNAME/g" /etc/kong/kong.yml
   sed -i -e "s/<PASSWORD>/$RDS_PASSWORD/g" /etc/kong/kong.yml
 #fi
